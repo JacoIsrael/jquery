3f31f247f1073c59f0f7a3f2b27f8a740ebdd160
Jacó Israel <jacoisraelfranco@gmail.com>
2018-05-07 11:13:33 -0300

    Add files via upload

---
+++ Jquery/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+<title></title>
+    <script src="jquery.3.3.1.js"></script>
+    <script>
+        $(document).ready(function(){
+            $("*").hide();
+        });
+    </script>
+</head>
+
+<body>
+
+    <h1>Pipipi Popopo</h1><br>
+
+    <button>Oculto</button><br>
+    <button>DesOculto</button><br><br>
+
+    <a>Um país em paz, é um país sem guerra</a><br><br>
+    <a>E um país sem guerra, é um país em paz</a><br>
+
+    <br>
+
+    <div style="background-color: #88ffc5; margin-right: 85%">
+        <a>SHAZAM</a>
+    </div>
+
+</body>
+</html>
